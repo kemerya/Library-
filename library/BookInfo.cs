51c05b075a10d2efe62141289d9ab8f16c2c98be
Kemerya <kemerzeynu00@gmail.com>
2023-11-24 09:33:02 +0300

    Add project files.

---
+++ library/BookInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    internal class BookInfo
+    {
+        private string author;
+        private string title;
+
+        public BookInfo(string author, string title)
+        {
+            this.author = author;
+            this.title = title;
+        }
+        //second constructor with constructor chaining
+        public BookInfo(string title): this(null, title) 
+        {
+            //constructor chaonning:calls the first constroctor with default author "null"
+        }
+        
+        //ToString() mthod overrode
+        public override string ToString() 
+        {
+            return $"{author} - {title}"; 
+        }
+    }
+}
---
+++ library/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create book objects using different constructors
+            BookInfo book1 = new BookInfo("j.k rowling","Harry potter");
+            BookInfo book2 = new BookInfo( "mokingbird");
+            BookInfo book3 = new BookInfo("George orwell","1984");
+            
+            //print book onformation using TOString()
+            console.WriteLine(book1.ToString());
+            console.WriteLine(book2.ToString());
+            console.Write(book3.ToString());
+        }
+    }
+}
